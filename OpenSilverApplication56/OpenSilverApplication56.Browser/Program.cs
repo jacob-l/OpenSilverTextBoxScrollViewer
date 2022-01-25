4d66cbb1a98d745c7f1a224d3eb6931ff94b3a72
Iakov Lilo <jlilo@hwdtech.com>
2022-01-25 16:29:15 +0600

    Add project files.

---
+++ OpenSilverApplication56/OpenSilverApplication56.Browser/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenSilverApplication56.Browser
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            var host = builder.Build();
+            await host.RunAsync();
+        }
+
+        public static void RunApplication()
+        {
+            Application.RunApplication(() =>
+            {
+                try
+                {
+                    var app = new OpenSilverApplication56.App();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+    }
+}
